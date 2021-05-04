b5bd998cd140ed92f31756e3bf92edaacf5233a7
Zaki <bonfohzaki@gmail.com>
2021-05-04 13:56:38 -0400

    first commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,31 @@
-import '../styles/globals.css'
+import NProgress from "nprogress"
+import Router    from "next/router"
+import "nprogress/nprogress.css"
 
-import { Provider } from 'react-redux'
+import { Provider }      from 'react-redux'
 import type { AppProps } from 'next/app'
 
-import store from '../app/store'
+import store             from '../app/store'
+import {
+   ChalkProvider,
+} from 'chalkui/dist/cjs'
+import theme             from '../theme'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <Provider store={store}>
-      <Component {...pageProps} />
-    </Provider>
-  )
+
+Router.events.on("routeChangeStart", () => {
+   NProgress.start()
+})
+Router.events.on("routeChangeComplete", () => NProgress.done())
+Router.events.on("routeChangeError", () => NProgress.done())
+
+function App({ Component, pageProps }: AppProps) {
+   return (
+      <Provider store={store}>
+         <ChalkProvider resetCSS theme={theme}>
+            <Component {...pageProps} />
+         </ChalkProvider>
+      </Provider>
+   )
 }
 
-export default MyApp
+export default App
---
+++ src/theme.tsx
@@ -0,0 +1,21 @@
+import { extendedTheme } from 'chalkui/dist/cjs'
+// import { createBreakpoints } from 'chalkui/chalk/Theme/Tools'
+
+// const fonts = { mono: `'Menlo', monospace` }
+
+// const breakpoints = createBreakpoints({
+//    sm: '40em',
+//    md: '52em',
+//    lg: '64em',
+//    xl: '80em',
+// })
+
+const theme = extendedTheme({
+   colors: {
+      black: '#16161D',
+   },
+   // fonts,
+   // breakpoints,
+})
+
+export default theme
